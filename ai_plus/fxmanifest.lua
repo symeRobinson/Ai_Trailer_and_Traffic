2d1a5f11bf50ce1c5b9c6065df33bee57fe13b53
Tobey Luciando <42970574+symeRobinson@users.noreply.github.com>
2020-06-30 17:50:15 -0500

    Update fxmanifest.lua

--- ai_plus/fxmanifest.lua
+++ ai_plus/fxmanifest.lua
@@ -1,12 +1,12 @@
 fx_version 'bodacious'
 game 'gta5'
 client_scripts {
-    'lua/client.lua',
-    'lua/shared.lua'
+    'lua/shared.lua',
+    'lua/client.lua'
 }
 server_scripts {
-    'lua/server.lua',
-    'lua/shared.lua'
+    'lua/shared.lua',
+    'lua/server.lua'
 }
 --[[
     CREATED BY Robinson#7901
